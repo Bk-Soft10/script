3804afc544bde2a1eeabf99137d0f592413e4ab6
abubakr <bksoft10@gmail.com>
2025-01-27 12:05:28 +0300

    update

--- copy_odoo_install.sh
+++ copy_odoo_install.sh
@@ -52,8 +52,8 @@ sudo apt install net-tools ssh git libpq-dev -y
 # Python3.12
 #--------------------------------------------------
 echo -e "\n---- Python3.12 ----"
-apt install software-properties-common -y
-add-apt-repository ppa:deadsnakes/ppa
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update -y
 #sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
 sudo apt install python3.12 python3.12-venv python3.12-dev -y
